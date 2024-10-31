602f9aa23d08be67f951ffc54933a811e4270f9c
DaddelZeit <daddelzeit.music@gmail.com>
2024-10-31 22:47:33 +0100

    re-add nodes

--- lua/vehicle/ngmp/sync/nodes.lua
+++ lua/vehicle/ngmp/sync/nodes.lua
@@ -5,22 +5,10 @@ local M = {
   author = "DaddelZeit (NGMP Official)"
 }
 
-local function doubleToBytes(num)
-  if not num then return end
-  return ffi.string(ffi.new("float[1]", num), 4)
-end
-
-local tmpFloat = ffi.new("float[1]")
-local function bytesToFloat(str)
-  ffi.copy(tmpFloat, str, 4)
-  return tmpFloat[0]
-end
-
 local function get()
   local data = {}
   for _, node in pairs(v.data.nodes) do
-    local tbl = obj:getNodePosition(node.cid)
-    data[#data + 1] = {doubleToBytes(tbl.x),doubleToBytes(tbl.y),doubleToBytes(tbl.z)}
+    data[#data + 1] = obj:getNodePosition(node.cid):toTable()
   end
   return next(data) and data or nil
 end
@@ -29,7 +17,7 @@ local tempVec = vec3()
 local function set(data)
   local i = 1
   for _, node in pairs(v.data.nodes) do
-    tempVec.x, tempVec.y, tempVec.z = bytesToFloat(data[i][1]), bytesToFloat(data[i][2]), bytesToFloat(data[i][3])
+    tempVec:set(data[i][1], data[i][2], data[i][3])
     obj:setNodePosition(node.cid, tempVec)
     i = i + 1
   end
