1774154a02704b08f9b1b6f8b0b33eff3f017856
DaddelZeit <daddelzeit.music@gmail.com>
2024-11-01 15:49:10 +0100

    maybe fix died

--- lua/vehicle/ngmp/sync/nodes.lua
+++ lua/vehicle/ngmp/sync/nodes.lua
@@ -8,9 +8,9 @@ local M = {
 local function get()
   local data = {}
   for _, node in pairs(v.data.nodes) do
+    local pos = obj:getNodePosition(node.cid)
     data[tonumber(node.cid)+1] = obj:getNodePosition(node.cid):toTable()
   end
-  dump("A")
   return data
 end
 
@@ -20,16 +20,14 @@ local function set(data)
     tempVec:set(nodePos[1], nodePos[2], nodePos[3])
     obj:setNodePosition(nodeCid-1, tempVec)
 
-    --[[
     local beam = v.data.beams[nodeCid]
     local beamPrecompression = beam.beamPrecompression or 1
     local deformLimit = type(beam.deformLimit) == 'number' and beam.deformLimit or math.huge
-    obj:setBeam(-1, beam.id1, beam.id2, beam.beamStrength, beam.beamSpring,
-                beam.beamDamp, type(beam.dampCutoffHz) == 'number' and beam.dampCutoffHz or 0,
-                beam.beamDeform, deformLimit, type(beam.deformLimitExpansion) == 'number' and beam.deformLimitExpansion or deformLimit,
-                beamPrecompression
+    local bid = obj:setBeam(-1, beam.id1, beam.id2, beam.beamStrength, beam.beamSpring,
+      beam.beamDamp, type(beam.dampCutoffHz) == 'number' and beam.dampCutoffHz or 0,
+      beam.beamDeform, deformLimit, type(beam.deformLimitExpansion) == 'number' and beam.deformLimitExpansion or deformLimit, type(beam.deformLimitStress) == 'number' and beam.deformLimitStress or math.huge,
+      beamPrecompression
     )
-    ]]
   end
 end
 
